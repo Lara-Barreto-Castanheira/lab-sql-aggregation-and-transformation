09d6728dd77e1160f12167e2d0047c9d98023c06
Lara-Barreto-Castanheira <lara.barretocastanheira@gmail.com>
2024-05-01 23:27:14 +0100

    done

---
+++ sql_lab3.sql
@@ -0,0 +1,90 @@
+#use sakila database
+USE sakila;
+
+#You need to use SQL built-in functions to gain insights relating to the duration of movies:
+##1.1 Determine the shortest and longest movie durations and name the values as max_duration and min_duration.
+SELECT MAX(length), MIN(length)
+FROM film;
+
+##1.2. Express the average movie duration in hours and minutes. Don't use decimals.
+##Hint: Look for floor and round functions.
+SELECT 
+    CONCAT(FLOOR(AVG(length) / 60), ' hours ',
+           ROUND(MOD(AVG(length), 60)),' minutes') 
+FROM film;
+
+#2.You need to gain insights related to rental dates:
+##2.1 Calculate the number of days that the company has been operating.
+##Hint: To do this, use the rental table, and the DATEDIFF() function to subtract the earliest date in the rental_date column from the latest date.
+SELECT DATEDIFF(MAX(rental_date), MIN(rental_date))
+FROM rental;
+
+#2.2 Retrieve rental information and add two additional columns to show the month and weekday of the rental. Return 20 rows of results.
+SELECT 
+    rental_id, 
+    rental_date, 
+    MONTH(rental_date), 
+    WEEKDAY(rental_date)
+FROM 
+    rental
+LIMIT 20;
+
+#2.3 Bonus: Retrieve rental information and add an additional column called DAY_TYPE with values 'weekend' or 'workday', depending on the day of the week.
+##Hint: use a conditional expression.
+SELECT 
+    rental_id, 
+    rental_date, 
+    CASE 
+        WHEN WEEKDAY(rental_date) IN (5,6) THEN 'weekend'
+        ELSE 'workday'
+    END AS DAY_TYPE
+FROM rental;
+
+#3.You need to ensure that customers can easily access information about the movie collection. 
+#To achieve this, retrieve the film titles and their rental duration. 
+#If any rental duration value is NULL, replace it with the string 'Not Available'. Sort the results of the film title in ascending order.
+
+SELECT 
+    title,
+    IFNULL(rental_duration, 'Not Available')
+FROM film
+ORDER BY title ASC;
+
+#CHALLENGE 2
+##Next, you need to analyze the films in the collection to gain some more insights. Using the film table, determine:
+##1.1 The total number of films that have been released.
+SELECT COUNT(*)
+FROM film;
+
+##1.2 The number of films for each rating.
+SELECT COUNT(*)
+FROM film
+GROUP BY rating;
+
+##1.3 The number of films for each rating, sorting the results in descending order of the number of films. 
+##This will help you to better understand the popularity of different film ratings and adjust purchasing decisions accordingly.
+SELECT rating, COUNT(*) AS number_of_films
+FROM film
+GROUP BY rating
+ORDER BY number_of_films DESC;
+
+#Using the film table, determine:
+##2.1 The mean film duration for each rating, and sort the results in descending order of the mean duration. 
+##Round off the average lengths to two decimal places. This will help identify popular movie lengths for each category.
+
+SELECT rating, AVG(length) AS mean_duration
+FROM film
+GROUP BY rating
+ORDER BY mean_duration DESC;
+
+##2.2 Identify which ratings have a mean duration of over two hours in order to help select films for customers who prefer longer movies.
+SELECT rating, AVG(length) AS mean_duration
+FROM film
+GROUP BY rating
+HAVING AVG(length) > 120;
+
+#Bonus: determine which last names are not repeated in the table actor.
+SELECT last_name
+FROM actor
+GROUP BY last_name
+HAVING COUNT(*) = 1;
